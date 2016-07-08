4b4a13aaf729513949812da9420a689aa6f995da
Ryan Mulkowsky <RyanMacBookAir@Ryans-MacBook-Air.local>
2016-07-08 14:46:01 -0400

    added alerts to game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,16 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert('Sorry, try again.');
+	} else if (cardThree === cardFour) {
+		alert('Sorry, try again.');
+	} else if (cardOne === cardThree) {
+		alert('You found a match!');
+	} else if (cardTwo === cardFour) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
