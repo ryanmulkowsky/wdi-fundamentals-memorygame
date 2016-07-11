5cbb0e271aa8f9b573447e2628de9e0dc13d8a87
Ryan Mulkowsky <RyanMacBookAir@Ryans-MacBook-Air.local>
2016-07-11 16:13:41 -0400

    added functions created DOM

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,31 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert('Sorry, try again.');
-	} else if (cardThree === cardFour) {
-		alert('Sorry, try again.');
-	} else if (cardOne === cardThree) {
-		alert('You found a match!');
-	} else if (cardTwo === cardFour) {
-		alert('You found a match!');
-	} else {
-		alert('Sorry, try again.');
+// if (cardOne === cardTwo) {
+	// alert('Sorry, try again.');
+	// } else if (cardThree === cardFour) {
+		// alert('Sorry, try again.');
+	// } else if (cardOne === cardThree) {
+		// alert('You found a match!');
+	// } else if (cardTwo === cardFour) {
+		// alert('You found a match!');
+	// } else {
+		// alert('Sorry, try again.');
+	// }
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+
+	for (var i = 0; i < cards.length; i++) {
+		
+		board.appendChild(cardElement);
+
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
 	}
+
+}
