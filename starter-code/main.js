76c5f1e83cdf947370e37b8f7d6a81f578fea9c1
Ryan Mulkowsky <RyanMacBookAir@Ryans-MacBook-Air.local>
2016-07-11 23:30:58 -0400

    finalized game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,6 +15,10 @@
 		// alert('Sorry, try again.');
 	// }
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
 var board = document.getElementById('game-board');
 
 function createBoard() {
@@ -26,6 +30,35 @@ function createBoard() {
 		var cardElement = document.createElement('div');
 
 		cardElement.className = 'card';
+
+		cardElement.setAttribute('data-card', cards[i]);
+
+		cardElement.addEventListener('click', isTwoCards);
 	}
 
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='kingspades.png' alt='King of Spades' />";
+			} else {
+				this.innerHTML = "<img src='queenhearts.png' alt='Queen of Hearts' />";
+			}
+
+	if (cardsInPlay.length === 2) {
+
+	isMatch(cardsInPlay);
+
+	cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 }
